b42a93fa3422d9a12285ed891b38d79a8669fd81
ferdjules <ucabjvo@ucl.ac.uk>
2020-03-22 01:34:34 +0100

    Added all existing files

---
+++ actions.js
@@ -0,0 +1,5 @@
+jQuery('.work-tab').click(function(){
+  // alert("The paragraph was clicked.");
+  // jQuery('.work-tab').css({"color": "green"});
+  // jQuery('.work-tab').addClass("active");
+});
